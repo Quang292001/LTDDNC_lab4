5c66c42ebf3876f6fad18bf9cd5c9926badcfdd0
Quang <nh.quang29200189@gmail.com>
2022-10-04 21:08:26 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import back from './assets/Vector.png'
+import mart from './assets/mart.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{backgroundColor:'#1BA9FF', flexDirection:'row', justifyContent:'space-between',alignItems:'center',paddingVertical:10}}>
+        <Image source={back} style={{marginLeft:20}}/>
+        <Text style={{color:"#fff", fontSize:20,fontWeight:'200'}}>Chat</Text>
+        <Image source={mart} style={{marginRight:20}}/>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
